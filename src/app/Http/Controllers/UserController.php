db726c2489cb9cbdd7ebb30516afe8c2510a282a
Oraz <oraznorlan2@gmail.com>
2024-11-28 01:15:59 +0500

    laravel passport implementation

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function store(UserRegisterRequest $request): JsonResponse
+    {
+        $new_user = $request->validated();
+
+        $user = User::create([
+            "username"   => $new_user['username'],
+            "first_name" => $new_user['first_name'],
+            "last_name"  => $new_user['last_name'],
+            "email"      => $new_user['email'],
+            "password"   => $new_user['password'],  // <----- Hashing throw user Model using setPasswordAttribute method
+        ]);
+
+        return response()->json([
+            "message" => "You successfully registered",
+            "data" => new UserResource($user),
+        ]);
+    }
+}
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +19,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -44,5 +47,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
+    public function findForPassport(string $username): User
+    {
+        return $this->where('username', $username)->first();
     }
 }
